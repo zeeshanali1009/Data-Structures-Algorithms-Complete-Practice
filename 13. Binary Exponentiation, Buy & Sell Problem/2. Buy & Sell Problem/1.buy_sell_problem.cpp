2092663a7920567a9fae58cbc68d9c7a6dc31c88
zeeshan1009 <zeeshan10091009@gmail.com>
2025-07-30 22:32:16 +0500

    buy and sell problem

---
+++ 13. Binary Exponentiation, Buy & Sell Problem/2. Buy & Sell Problem/1.buy_sell_problem.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+int maxProfit(vector<int>& prices) {
+    int minPrice = INT_MAX;
+    int maxProfit = 0;
+
+    for (int price : prices) {
+        if (price < minPrice) {
+            minPrice = price; // Update min buying price
+        } else {
+            maxProfit = max(maxProfit, price - minPrice); // Try selling here
+        }
+    }
+    return maxProfit;
+}
+
+int main() {
+    vector<int> prices = {7, 1, 5, 3, 6, 4};
+    cout << "Maximum Profit: " << maxProfit(prices) << endl;
+    return 0;
+}
